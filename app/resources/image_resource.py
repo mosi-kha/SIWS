3649f3aa110fd59fce817d4d0afc903bd15e8eda
mostafa khaki <mostafa@cheetagroup.com>
2020-03-01 22:39:03 +0330

    Change image resource

---
+++ app/resources/image_resource.py
@@ -0,0 +1,19 @@
+import falcon
+from falcon import Request, Response
+
+from app.controllers.image_controller import ImageController
+
+
+class ImageResource:
+    def on_get(self, req: Request, resp: Response, *args, **kwargs):
+        resp.status = falcon.HTTP_200
+        resp.body = "use POST method to send Image, Allowed Format: png, jpg, jpeg, gif"
+
+    @falcon.before(ImageController().validate_image_type)
+    def on_post(self, req: Request, resp: Response, *args, **kwargs):
+        try:
+            response = ImageController().get_image_size(req.stream, req.content_type)
+            resp.media = response
+            resp.status = falcon.HTTP_200
+        except Exception as ex:
+            raise falcon.HTTPBadRequest('bad request', ex.__str__())
