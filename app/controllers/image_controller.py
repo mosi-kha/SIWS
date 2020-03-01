4bd73778c8b632e55391e8968a7566beaaefa3d0
mostafa khaki <mostafa@cheetagroup.com>
2020-03-01 23:28:35 +0330

    use stream bytes convert instead of file

--- app/controllers/image_controller.py
+++ app/controllers/image_controller.py
@@ -1,30 +1,27 @@
 import falcon
-from PIL import Image
-import mimetypes
 
-ALLOWED_IMAGE_TYPES = {falcon.MEDIA_JPEG, falcon.MEDIA_GIF, falcon.MEDIA_PNG, 'image/jpg'}
+from io import BytesIO
+from PIL import Image
 
 
 class ImageController:
     _CHUNK_SIZE_BYTES = 4096
+    _ALLOWED_IMAGE_TYPES = {falcon.MEDIA_JPEG, falcon.MEDIA_GIF, falcon.MEDIA_PNG, 'image/jpg'}
 
     def validate_image_type(self, req, resp, resource, *params):
-        if req.content_type not in ALLOWED_IMAGE_TYPES:
+        if req.content_type not in self._ALLOWED_IMAGE_TYPES:
             msg = 'Image type not allowed. Must be PNG, JPEG, JPG, or GIF'
             raise falcon.HTTPBadRequest('Bad request', msg)
 
     def get_image_size(self, image_stream, content_type):
-        ext = mimetypes.guess_extension(content_type)
-        with open(f'image{ext}', 'wb') as file:
-            while True:
-                chunk = image_stream.read(self._CHUNK_SIZE_BYTES)
-                if not chunk:
-                    break
+        image_bytes = []
+        while True:
+            chunk = image_stream.read(self._CHUNK_SIZE_BYTES)
+            if not chunk:
+                break
+            image_bytes.append(chunk)
 
-                file.write(chunk)
-
-        image = Image.open(f'image{ext}')
-
+        image = Image.open(BytesIO(b''.join(image_bytes)))
         return {
             "format": image.format,
             "height": image.height,
