a6b54dfeb8d528d8afafb6363df98eab91805066
mostafa khaki <mostafa@cheetagroup.com>
2020-03-02 09:56:31 +0330

    Update test

--- test/test_api.py
+++ test/test_api.py
@@ -1,59 +1,59 @@
 import requests
-from PIL import Image, ImageDraw
+
+from PIL import Image
 from random import randint
-from io import BytesIO
-import json
 
 
-# NOTE:
-
-def test_api():
+class TestAPI:
     """
         in This test, we create random image and simulate POST method
         Pillow Don't Support JPG Format
-    :return:
     """
-    # PNG
-    image = Image.new('RGB', size=(randint(100, 1000), randint(100, 1000)), color=(200, 100, 100))
-    image.save('image', 'png')
 
-    resp = requests.post(
-        url='http://localhost:8000',
-        data=open('image', 'rb'),
-        headers={'content-type': 'image/png'}
-    )
-    assert resp.status_code == 200
-    resp = resp.json()
-    assert resp['width'] == image.width
-    assert resp['height'] == image.height
-    assert resp['format'] == 'PNG'
+    # PNG
+    def test_png(self):
+        image = Image.new('RGB', size=(randint(100, 1000), randint(100, 1000)), color=(200, 100, 100))
+        image.save('image', 'png')
+
+        resp = requests.post(
+            url='http://localhost:8000',
+            data=open('image', 'rb'),
+            headers={'content-type': 'image/png'}
+        )
+        assert resp.status_code == 200
+        resp = resp.json()
+        assert resp['width'] == image.width
+        assert resp['height'] == image.height
+        assert resp['format'] == 'PNG'
 
     # GIF
-    image = Image.new('RGB', size=(randint(100, 1000), randint(100, 1000)), color=(200, 100, 100))
-    image.save('image', 'GIF')
+    def test_gif(self):
+        image = Image.new('RGB', size=(randint(100, 1000), randint(100, 1000)), color=(200, 100, 100))
+        image.save('image', 'GIF')
 
-    resp = requests.post(
-        url='http://localhost:8000',
-        data=open('image', 'rb'),
-        headers={'content-type': 'image/png'}
-    )
-    assert resp.status_code == 200
-    resp = resp.json()
-    assert resp['width'] == image.width
-    assert resp['height'] == image.height
-    assert resp['format'] == 'GIF'
+        resp = requests.post(
+            url='http://localhost:8000',
+            data=open('image', 'rb'),
+            headers={'content-type': 'image/gif'}
+        )
+        assert resp.status_code == 200
+        resp = resp.json()
+        assert resp['width'] == image.width
+        assert resp['height'] == image.height
+        assert resp['format'] == 'GIF'
 
     # JPEG
-    image = Image.new('RGB', size=(randint(100, 1000), randint(100, 1000)), color=(200, 100, 100))
-    image.save('image', 'JPEG')
+    def test_jpeg(self):
+        image = Image.new('RGB', size=(randint(100, 1000), randint(100, 1000)), color=(200, 100, 100))
+        image.save('image', 'JPEG')
 
-    resp = requests.post(
-        url='http://localhost:8000',
-        data=open('image', 'rb'),
-        headers={'content-type': 'image/png'}
-    )
-    assert resp.status_code == 200
-    resp = resp.json()
-    assert resp['width'] == image.width
-    assert resp['height'] == image.height
-    assert resp['format'] == 'JPEG'
+        resp = requests.post(
+            url='http://localhost:8000',
+            data=open('image', 'rb'),
+            headers={'content-type': 'image/jpeg'}
+        )
+        assert resp.status_code == 200
+        resp = resp.json()
+        assert resp['width'] == image.width
+        assert resp['height'] == image.height
+        assert resp['format'] == 'JPEG'
